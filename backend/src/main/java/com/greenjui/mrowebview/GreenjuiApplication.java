645bd2ceff36ba644addec879d6e7fd5ea627db1
kimsumin <tnals1564@naver.com>
2024-01-02 17:58:12 +0900

    모바일 mro 개발환경셋팅

---
+++ backend/src/main/java/com/greenjui/mrowebview/GreenjuiApplication.java
@@ -0,0 +1,12 @@
+package com.greenjui.mrowebview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenjuiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenjuiApplication.class, args);
+    }
+}
